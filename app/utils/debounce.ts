83cf9f3feaa973058d03de97d2f2291d754ce994
antosh <tomatomoving@mail.ru>
2026-09-01 02:41:10 +0300

    feat: add debounce util

---
+++ app/utils/debounce.ts
@@ -0,0 +1,16 @@
+export function debounce<T extends any[], R>(
+  func: (this: any, ...args: T) => R,
+  delay: number,
+): (this: any, ...args: T) => void {
+  let timeoutId: ReturnType<typeof setTimeout> | undefined
+
+  return function (this: any, ...args: T): void {
+    const context = this
+    if (timeoutId) {
+      clearTimeout(timeoutId)
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(context, args)
+    }, delay)
+  }
+}
