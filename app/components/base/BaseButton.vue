37609abc2b7ac35fa33ecee9ca912e062932731c
antosh <tomatomoving@mail.ru>
2026-08-31 19:07:31 +0300

    feat: add base button component

---
+++ app/components/base/BaseButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+interface Props {
+  type?: 'success' | 'danger' | 'info' | 'primary'
+}
+const { type = 'primary' } = defineProps<Props>()
+</script>
+
+<template>
+  <button class="button" :class="`button--${type}`">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss">
+.button {
+  font-size: var(--text-md);
+  padding: var(--spacing-sm);
+  border: var(--btn-border-color);
+  border-radius: var(--radius-sm);
+}
+</style>
