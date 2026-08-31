be6eb06c64a7ce0138acf0c27c7835238e3e6af5
antosh <tomatomoving@mail.ru>
2026-08-31 19:50:49 +0300

    feat: add size gradation to base button component

--- app/components/base/BaseButton.vue
+++ app/components/base/BaseButton.vue
@@ -1,21 +1,33 @@
 <script setup lang="ts">
 interface Props {
   type?: 'success' | 'danger' | 'info' | 'primary'
+  size?: 'small' | 'medium' | 'large'
 }
-const { type = 'primary' } = defineProps<Props>()
+const { type = 'primary', size = 'medium' } = defineProps<Props>()
 </script>
 
 <template>
-  <button class="button" :class="`button--${type}`">
+  <button class="button" :class="`button--${type} button--${size}`">
     <slot></slot>
   </button>
 </template>
 
 <style scoped lang="scss">
 .button {
-  font-size: var(--text-md);
-  padding: var(--spacing-sm);
   border: var(--btn-border-color);
   border-radius: var(--radius-sm);
+
+  &--small {
+    padding: var(--spacing-2xs) var(--spacing-xs);
+    font-size: var(--text-xs);
+  }
+  &--medium {
+    padding: var(--spacing-xs) var(--spacing-sm);
+    font-size: var(--text-md);
+  }
+  &--large {
+    padding: var(--spacing-sm) var(--spacing-md);
+    font-size: var(--text-2xl);
+  }
 }
 </style>
