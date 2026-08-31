e6bd09762eef0e78c7840ff33575903c74df4a0c
antosh <tomatomoving@mail.ru>
2026-08-31 17:46:35 +0300

    feat: set base scss system

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  ssr: false,
+  css: ["~/assets/scss/main.scss"],
+  postcss: {
+    plugins: {
+      autoprefixer: {}, // Автоматически включает Autoprefixer
+    },
+  },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "~/assets/scss/_variables.scss" as *;`,
+        },
+      },
+    },
+  },
+});
